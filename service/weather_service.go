ce47b83417230887d13c3abd0af29d74f1738d46
Ilya Stone <tnt228337@gmail.com>
2023-08-01 17:25:40 +0300

    create func get X2 and func build

--- service/weather_service.go
+++ service/weather_service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/xlzpm/web-weather/config"
 )
 
@@ -17,4 +19,32 @@ type HttpClient interface {
 type SyncWeatherService struct {
 	config *config.ServerConfiguration
 	Client HttpClient
+}
+
+func CreateWeatherService(config *config.ServerConfiguration, client HttpClient) WeatherService {
+	ws := &SyncWeatherService{config: config, Client: client}
+	return ws
+}
+
+func (wc *SyncWeatherService) GetWeather(cities []string) (*WeatherReports, error) {
+	result := WeatherReports{
+		Reports: make(map[string]*CityReport),
+	}
+
+	for _, city := range cities {
+		report, err := ws.GetSingleCityWeather(city)
+
+	}
+}
+
+func (wc *SyncWeatherService) GetSingleCityWeather(city string) (*CityReport, error) {
+	log.Debugf("Fetching weather for city %s", city)
+
+	fullUrl := wc.buildUrl(city)
+
+	log.Info(fullUrl)
+}
+
+func (wc *SyncWeatherService) buildUrl(city string) string {
+	return fmt.Sprintf("%s%s&%s=%s", wc.config.API, city, wc.config.APIKeyParam, wc.config.APIKey)
 }
